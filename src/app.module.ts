d95a9821bda31bfa700d45e3f6fa2af603f358bb
ykgautam09 <yateeshgautam1357@gmail.com>
2022-02-20 14:43:15 +0530

    mongodb with mongoose and dotenv setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ContactModule } from './contact/contact.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import 'dotenv/config';
 
 @Module({
-  imports: [ContactModule],
+  imports: [
+    ContactModule,
+    MongooseModule.forRoot(process.env.MONGODB_CONNECTION_STRING),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
