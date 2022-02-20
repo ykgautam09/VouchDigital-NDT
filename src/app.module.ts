fe6fd3d071fbc787ce5243b8f86d0f6ea492254c
ykgautam09 <yateeshgautam1357@gmail.com>
2022-02-21 00:18:46 +0530

    jwt project dependency configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ContactModule } from './contact/contact.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
 import 'dotenv/config';
 
 @Module({
   imports: [
     ContactModule,
     MongooseModule.forRoot(process.env.MONGODB_CONNECTION_STRING),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
